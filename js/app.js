85462e72998571ed76afa983f94f434bf7b9beca
Michael Codner <mhcodner@gmail.com>
2016-10-08 19:23:27 +0200

    implement partials

--- js/app.js
+++ js/app.js
@@ -1,2 +1,16 @@
 var baseThemeURI = themeSettings.themeUri;
-var SBM = angular.module('SBM', ['ngRoute']);
+var SBM = angular.module('SBM', ['ngRoute'])
+    .config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
+        /**
+         *    Configure routes
+         */
+        $routeProvider
+            .when('/', {
+                templateUrl: baseThemeURI + '/partials/index.html'
+            })
+
+        /**
+         *    Remove # from the URL with $locationProvider
+         */
+        $locationProvider.html5Mode(true).hashPrefix('!');
+    }])
