bcfb3203f50cd8d14d7a9f2d5821c2fe3d64040a
Michael Codner <mhcodner@gmail.com>
2016-10-08 18:39:26 +0200

    create grunt tasks

---
+++ gruntfile.js
@@ -0,0 +1,56 @@
+module.exports = function (grunt) {
+    require("matchdep").filterDev("grunt-*").forEach(grunt.loadNpmTasks);
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        cssmin: {
+            sitecss: {
+                options: {
+                    root: './'
+                },
+                files: {
+                    'css/bundle.css': [
+                        'bower_components/materialize/bin/materialize.css',
+                        'css/main.css'
+                    ]
+                }
+            }
+        },
+        uglify: {
+            options: {
+                compress: true,
+                mangle: false
+            },
+            applib: {
+                src: [
+                    'bower_components/jquery/dist/jquery.min.js',
+                    'bower_components/angular/angular.min.js',
+                    'bower_components/angular-route/angular-route.min.js',
+                    'bower_components/materialize/bin/materialize.js',
+                    'js/app.js'
+                ],
+                dest: 'js/bundle.js'
+            }
+        },
+        watch: {
+            scripts: {
+                files: ['**/*.js', '**/*.css', '!css/bundle.css', '!js/bundle.js'],
+                tasks: ['uglify', 'cssmin'],
+                options: {
+                    spawn: false
+                }
+            }
+        },
+        copy: {
+            main: {
+                expand: true,
+                cwd: 'bower_components/materialize/fonts/',
+                src: '**',
+                dest: 'fonts/',
+                filter: 'isFile'
+            }
+        }
+    });
+    // Default task.
+    grunt.registerTask('default', ['uglify', 'cssmin']);
+};
