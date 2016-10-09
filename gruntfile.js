55b805d472334cc958b469eac3ad85a8be4ed949
Michael Codner <mhcodner@gmail.com>
2016-10-09 13:38:33 +0200

    initialise menu

--- gruntfile.js
+++ gruntfile.js
@@ -24,7 +24,8 @@ module.exports = function (grunt) {
                     'bower_components/angular/angular.min.js',
                     'bower_components/angular-route/angular-route.min.js',
                     'bower_components/materialize/bin/materialize.js',
-                    'js/app.js'
+                    'js/app.js',
+                    'js/main.js'
                 ],
                 dest: 'js/bundle.js'
             }
---
+++ js/main.js
@@ -0,0 +1,8 @@
+jQuery(document).ready(function ($) {
+    $("body").on('click', 'ul.side-nav li a', function () {
+        if ($(window).width() <= 992) {
+            $('.button-collapse').sideNav('hide');
+        }
+    });
+    $(".button-collapse").sideNav();
+});
