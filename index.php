21b90d3b6d9347ed873fe1366d62b7ec41b0d1bd
Michael Codner <mhcodner@gmail.com>
2016-11-04 15:43:12 +0100

    update to font awesome 4.7

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <title>Solar Bundeena Maianbar</title>
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <?php ob_start('fix_links'); wp_head(); ?>
 </head>
 <body ng-cloak>
