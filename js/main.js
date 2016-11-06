ee3648e04cdcc8703fb5d9c6c4c61955af90b4b8
Michael Codner <mhcodner@gmail.com>
2016-11-06 20:53:41 +0100

    make local links relative using JS instead

--- js/main.js
+++ js/main.js
@@ -18,21 +18,24 @@ function targetLinks(htmlText) {
     div.innerHTML = htmlText;
     anchors = div.getElementsByTagName('a');
     for (var i = 0; i < anchors.length; i++) {
+        var tmp = document.createElement("div");
+        tmp.appendChild(anchors[i].cloneNode(true));
+        var newAnchor = tmp.innerHTML;
+        // Make local links relative
+        newAnchor = newAnchor.replace(themeSettings.siteURL, '');
         // Most links that need to be "fixed" will be attachments
         if (anchors[i].href.indexOf("/wp-content/uploads/") != -1) {
-            var tmp = document.createElement("div");
-            tmp.appendChild(anchors[i].cloneNode(true));
             // Replaces the link with a link that opens in new tab/window which works with angular routing
-            var newAnchor = tmp.innerHTML.replace('">', '" target="_blank">');
-            // Get the extension of the href accounting for both new and old microsoft office extensions
-            var extension = anchors[i].href.substring(anchors[i].href.length - 4, anchors[i].href.length).replace('.', '');
-            // Check if the extension of the anchor is a document
-            if ("docxpdfxlsxpptx".indexOf(extension) != -1) {
-                // Add text document icon to the anchor text
-                newAnchor = newAnchor.replace(anchors[i].text, '<i class="fa fa-file-text"></i> ' + anchors[i].text);
-            }
-            div.innerHTML = div.innerHTML.replace(tmp.innerHTML, newAnchor);
+            newAnchor = newAnchor.replace('">', '" target="_blank">');
         }
+        // Get the extension of the href accounting for both new and old microsoft office extensions
+        var extension = anchors[i].href.substring(anchors[i].href.length - 4, anchors[i].href.length).replace('.', '');
+        // Check if the extension of the anchor is a document
+        if ("docxpdfxlsxpptx".indexOf(extension) != -1) {
+            // Add text document icon to the anchor text
+            newAnchor = newAnchor.replace(anchors[i].text, '<i class="fa fa-file-text"></i> ' + anchors[i].text);
+        }
+        div.innerHTML = div.innerHTML.replace(tmp.innerHTML, newAnchor);
     }
     return div.innerHTML;
 }
